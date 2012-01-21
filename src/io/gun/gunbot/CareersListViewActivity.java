a01164c781e91efed02bab95c1881739629e6670
Trevor Basinger <trevor.basinger@gmail.com>
2012-01-21 17:33:22 -0600

    Reorganized the IndividualItemActivity so that it can support displaying more than one type of data. Implemented FreelanceListViewActivity's OnClickListener so that it will populate the IndividualItemActivity with data.

--- src/io/gun/gunbot/CareersListViewActivity.java
+++ src/io/gun/gunbot/CareersListViewActivity.java
@@ -38,79 +38,77 @@ public class CareersListViewActivity extends Activity{
   View lastTouched;
 
   @Override
-    public void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-      requestWindowFeature(Window.FEATURE_NO_TITLE);
-      setContentView(R.layout.detailed_menu_list);    	
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    setContentView(R.layout.detailed_menu_list);    	
 
-      TextView title = (TextView)findViewById(R.id.title_bar);
-      title.setText("Gun.io - Careers");
+    TextView title = (TextView)findViewById(R.id.title_bar);
+    title.setText("Gun.io - Careers");
 
-      ArrayList<Career> l = new ArrayList<Career>();
-      final Careers ca = new Careers();
-      Career career = ca.new Career();
-      career.setTitle("Loading..");
-      City lo = new City();
-      lo.setName("Please wait..");
-      career.setCity(lo);
-      career.setCompany_name("");
-      l.add(career);
-      ca.setObjects(l);
+    ArrayList<Career> l = new ArrayList<Career>();
+    final Careers ca = new Careers();
+    Career career = ca.new Career();
+    career.setTitle("Loading..");
+    City lo = new City();
+    lo.setName("Please wait..");
+    career.setCity(lo);
+    career.setCompany_name("");
+    l.add(career);
+    ca.setObjects(l);
 
-      //Setup the adapter views;
-      adapter = new CareersArrayListAdapter(this, R.layout.list_view_row);
-      adapter.setItems(ca.getCareers());
-      adapter.setContext(getBaseContext());
-      adapter.setParent(this);
-      lv = (ListView) findViewById(R.id.list);
-      lv.setAdapter(adapter);
+    //Setup the adapter views;
+    adapter = new CareersArrayListAdapter(this, R.layout.list_view_row);
+    adapter.setItems(ca.getCareers());
+    adapter.setContext(getBaseContext());
+    adapter.setParent(this);
+    lv = (ListView) findViewById(R.id.list);
+    lv.setAdapter(adapter);
 
-      //Create an AsyncTask to update that data;
-      getDataTask = new GetCareersDataTask();
-      getDataTask.setContext(this);
-      getDataTask.setAdapter(adapter);
-      getDataTask.execute(careersURL, careersURL);
+    //Create an AsyncTask to update that data;
+    getDataTask = new GetCareersDataTask();
+    getDataTask.setContext(this);
+    getDataTask.setAdapter(adapter);
+    getDataTask.execute(careersURL, careersURL);
 
-      final CareersListViewActivity context = CareersListViewActivity.this;
-      lv.setOnItemClickListener(new OnItemClickListener() {
+    final CareersListViewActivity context = CareersListViewActivity.this;
+    lv.setOnItemClickListener(new OnItemClickListener() {
 
-        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+      public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 
-          final Intent i = new Intent(context, IndividualItemActivity.class);
-          Career c = ca.getCareers().get(arg2);
-          c = (Career) adapter.getCareer(arg2);
+        final Intent i = new Intent(context, IndividualItemActivity.class);
+        Career c = ca.getCareers().get(arg2);
+        c = (Career) adapter.getCareer(arg2);
 
-          if(c.getTitle().equals("Loading..")){
-            return;
-          }
+        if(c.getTitle().equals("Loading..")){
+          return;
+        }
 
-          String mkData = "###"+c.getTitle() + "\n";
-          if( c.getJob_mkd() != null )
-            mkData += c.getJob_mkd() + "\n";
-          if( c.getSkills_mkd() != null )
-            mkData += c.getSkills_mkd() + "\n";
+        boolean hasMarkup = ((c.getJob_mkd() != null && c.getJob_mkd() != "")
+          && ( c.getAbout_mkd() != null && c.getAbout_mkd() != "" ));
 
-          i.putExtra("has_markup", true);
-          i.putExtra("mdv_data", mkData);
-          i.putExtra("post_type", IndividualItemActivity.POST_TYPE_CAREER);
-          i.putExtra("title", c.getTitle());
-          i.putExtra("company_name", c.getCompany_name());
+        if(hasMarkup) {
           i.putExtra("job_description", c.getJob_mkd());
           i.putExtra("about", c.getAbout_mkd());
           i.putExtra("skills", c.getSkills_mkd());
-          i.putExtra("apply_url", c.getHomepage());
-          i.putExtra("id", c.getId());
-          i.putExtra("slug", c.getSlug());
+        } else {
+          i.putExtra("job_description", c.getJob());
+          i.putExtra("about", c.getAbout());
+          i.putExtra("skills", c.getSkills());
+        }
+        i.putExtra("post_type", IndividualItemActivity.POST_TYPE_CAREER);
+        i.putExtra("has_markup", hasMarkup);
+        i.putExtra("title", c.getTitle());
+        i.putExtra("company_name", c.getCompany_name());
+        i.putExtra("city", c.getCity().getName());
+        i.putExtra("apply_url", c.getHomepage());
+        i.putExtra("id", c.getId());
+        i.putExtra("slug", c.getSlug());
 
-          startActivity(i); 
+        startActivity(i); 
 
-        }});
+      }});
 
-    }
-
-  public void onResume() {
-    super.onResume();
-  }    
-
+  }
 }
--- src/io/gun/gunbot/IndividualItemActivity.java
+++ src/io/gun/gunbot/IndividualItemActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,7 @@ import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
 import android.view.View.OnTouchListener;
 import android.view.Window;
+import android.webkit.WebView;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -42,47 +44,154 @@ public class IndividualItemActivity extends Activity {
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     requestWindowFeature(Window.FEATURE_NO_TITLE);
-    setContentView(R.layout.detailed_markup_item);
-    Intent i = this.getIntent(); 
-    
-    if ( i.getBooleanExtra("has_markup", false) ) {
-      LinearLayout llayout = (LinearLayout) findViewById(R.id.info_layout);
-      MarkdownView mdv = new MarkdownView(this);
-      mdv.setBackgroundColor(Color.TRANSPARENT);
-      if( llayout == null ) {
-        Log.d("gun.io","Couldn't fetch layout view");
-        finish();
-      }
-      llayout.addView(mdv, 0, new ViewGroup.LayoutParams(-1, -1));
-      String mdData = i.getStringExtra("mdv_data");
-      mdv.loadMarkDownData(mdData);
-      return;
-    }
-
     setContentView(R.layout.detailed_item);
+    
+    final Intent i = this.getIntent(); 
+    final LinearLayout llayout = (LinearLayout) findViewById(R.id.info_layout);
 
+    WebView wView;    // This is the view that the data is loaded into
+    String data = ""; // This is the container for the data
 
-    /** Assign layouts to views. */
-    TextView title = (TextView)findViewById(R.id.title);
-    TextView header = (TextView)findViewById(R.id.title_bar);
+    /**
+     * This code below is sloppy, but I wanted to stick
+     * with the original design of keeping a single activity to handle
+     * all the data in case there were future design ideas based around
+     * this. I also thought about moving code away from the onCreate
+     * method but I figured it would be best not to abstract this
+     * further until a final decision regarding this design is made.
+     */
 
-    TextView title2 = (TextView)findViewById(R.id.title2);
+    final int type = i.getIntExtra("post_type", -1);
+    Log.d("gun.io","Type is: "+type);
+    if(type == POST_TYPE_CAREER) { // BEGIN CAREER LAYOUT
+      data = "##"+i.getStringExtra("title") + "\n"
+        + "###" + i.getStringExtra("company_name") + "\n"
+        + "###" + i.getStringExtra("city") + "\n"
+        + i.getStringExtra("about") + "\n"
+        + i.getStringExtra("job_description") +"\n"
+        + i.getStringExtra("skills") + "\n";
 
-    TextView header1 = (TextView)findViewById(R.id.header1);
-    TextView text1 = (TextView)findViewById(R.id.text1);
+      /** If the intent contains markup data */
+      /** Load the markup view */
+      if ( i.getBooleanExtra("has_markup", false) ) { 
+        // Prepare the markup view
+        MarkdownView mdv = new MarkdownView(this);
+        mdv.loadMarkDownData(data);
+        wView = mdv;
+      } else {
+        TextView title = new TextView(this);
+        TextView header = new TextView(this);
+        TextView title2 =  new TextView(this);
+        wView = new WebView(this);
 
-    TextView header2 = (TextView)findViewById(R.id.header2);
-    TextView text2 = (TextView)findViewById(R.id.text2);
+        title.setText(i.getStringExtra("title"));
+        title.setTextSize(22.0f);
+        title.setTextColor(Color.BLACK);
+        title.setPadding(10,0,10,10);
+        llayout.addView(title, -1, -1);
 
-    TextView header3 = (TextView)findViewById(R.id.header3);
-    TextView text3 = (TextView)findViewById(R.id.text3);
+        title2.setText(i.getStringExtra("company_name"));
+        title2.setTextSize(20.0f);
+        title2.setTextColor(Color.BLACK);
+        title2.setPadding(10,0,10,10);
+        llayout.addView(title2, -1, -1);
 
-    TextView apply_button = (TextView)findViewById(R.id.apply_button);
-    apply_button.setOnClickListener(new ApplyButtonListener());
+        header.setText(i.getStringExtra("city"));
+        header.setTextSize(20.0f);
+        header.setTextColor(Color.BLACK);
+        header.setPadding(10,0,10,10);
+        llayout.addView(header, -1, -1);
 
-    accounts = AccountManager.get(this).getAccounts();
-    TextView email_button = (TextView)findViewById(R.id.remind_button);
-    email_button.setOnClickListener(new EmailButtonListener());
+        wView.loadData(data, "text/html", "UTF-8");
+      }
+      wView.setBackgroundColor(Color.TRANSPARENT);
+      llayout.addView(wView, -1, -1);
+
+      /** Add an apply button */
+      LayoutInflater inflater = this.getLayoutInflater();
+      View apply_button= inflater.inflate(R.layout.apply_to_job, null);
+      apply_button.setOnClickListener(new ApplyButtonListener());
+      llayout.addView(apply_button, -1, -1);
+
+      accounts = AccountManager.get(this).getAccounts();
+      TextView email_button = (TextView)findViewById(R.id.remind_button);
+      email_button.setOnClickListener(new EmailButtonListener());
+    } // END CAREER LAYOUT
+    if(type == POST_TYPE_CONTRACT) { // BEGIN CONTRACT LAYOUT
+
+      data = "##"+i.getStringExtra("title") + "\n"
+        + "###$" + i.getIntExtra("value", -1) + "\n"
+        + i.getStringExtra("description") + "\n"
+        + i.getStringExtra("requirements") +"\n"
+        + i.getStringExtra("proof") + "\n";
+
+      /** If the intent contains markup data */
+      /** Load the markup view */
+      if ( i.getBooleanExtra("has_markup", false) ) { 
+        // Prepare the markup view
+        MarkdownView mdv = new MarkdownView(this);
+        mdv.loadMarkDownData(data);
+        wView = mdv;
+      } else {
+        TextView title = new TextView(this);
+        TextView header = new TextView(this);
+        TextView title2 =  new TextView(this);
+        wView = new WebView(this);
+
+        title.setText(i.getStringExtra("title"));
+        title.setTextSize(22.0f);
+        title.setTextColor(Color.BLACK);
+        title.setPadding(10,0,10,10);
+        llayout.addView(title, -1, -1);
+
+        title2.setText(i.getStringExtra("value"));
+        title2.setTextSize(20.0f);
+        title2.setTextColor(Color.BLACK);
+        title2.setPadding(10,0,10,10);
+        llayout.addView(title2, -1, -1);
+
+        wView.loadData(data, "text/html", "UTF-8");
+      }
+      wView.setBackgroundColor(Color.TRANSPARENT);
+      llayout.addView(wView, -1, -1);
+    } // END CONTRACT LAYOUT
+    if(type == POST_TYPE_OPEN) { // BEGIN OPEN LAYOUT
+      data = "##"+i.getStringExtra("title") + "\n"
+        + "###$" + i.getIntExtra("value", -1) + "\n"
+        + i.getStringExtra("description") + "\n"
+        + i.getStringExtra("requirements") +"\n"
+        + i.getStringExtra("proof") + "\n";
+
+      /** If the intent contains markup data */
+      /** Load the markup view */
+      if ( i.getBooleanExtra("has_markup", false) ) { 
+        // Prepare the markup view
+        MarkdownView mdv = new MarkdownView(this);
+        mdv.loadMarkDownData(data);
+        wView = mdv;
+      } else {
+        TextView title = new TextView(this);
+        TextView header = new TextView(this);
+        TextView title2 =  new TextView(this);
+        wView = new WebView(this);
+
+        title.setText(i.getStringExtra("title"));
+        title.setTextSize(22.0f);
+        title.setTextColor(Color.BLACK);
+        title.setPadding(10,0,10,10);
+        llayout.addView(title, -1, -1);
+
+        title2.setText(i.getStringExtra("value"));
+        title2.setTextSize(20.0f);
+        title2.setTextColor(Color.BLACK);
+        title2.setPadding(10,0,10,10);
+        llayout.addView(title2, -1, -1);
+
+        wView.loadData(data, "text/html", "UTF-8");
+      }
+      wView.setBackgroundColor(Color.TRANSPARENT);
+      llayout.addView(wView, -1, -1);
+    } // END OPEN LAYOUT
   }
 
   private class EmailButtonListener implements OnClickListener {
