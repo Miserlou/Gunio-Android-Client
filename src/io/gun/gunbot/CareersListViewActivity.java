3cad00f66734441d724217856f402e034176459f
Trevor Basinger <trevor.basinger@gmail.com>
2012-01-20 20:54:07 -0600

    Got markup views working in Careers portion of app.

--- src/io/gun/gunbot/CareersListViewActivity.java
+++ src/io/gun/gunbot/CareersListViewActivity.java
@@ -23,84 +23,94 @@ import android.widget.TextView;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class CareersListViewActivity extends Activity{
-    
-    CareersArrayListAdapter adapter;
-    ListView lv;
-    ArrayList<ArrayList<HashMap<String, String>>> mal;
-    Context c;
-    String u;
-    ImageView headerButton;
-    Animation rotate;
-    GetCareersDataTask getDataTask;
-    
-    String careersURL = "http://gun.io/secretapi/v1/careers/?format=json";
-    
-    View lastTouched;
-    
-    @Override
+
+  CareersArrayListAdapter adapter;
+  ListView lv;
+  ArrayList<ArrayList<HashMap<String, String>>> mal;
+  Context c;
+  String u;
+  ImageView headerButton;
+  Animation rotate;
+  GetCareersDataTask getDataTask;
+
+  String careersURL = "http://gun.io/secretapi/v1/careers/?format=json";
+
+  View lastTouched;
+
+  @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.detailed_menu_list);    	
-        
-        TextView title = (TextView)findViewById(R.id.title_bar);
-        title.setText("Gun.io - Careers");
-        
-        ArrayList<Career> l = new ArrayList<Career>();
-        final Careers ca = new Careers();
-        Career career = ca.new Career();
-        career.setTitle("Loading..");
-        City lo = new City();
-        lo.setName("Please wait..");
-        career.setCity(lo);
-        career.setCompany_name("");
-        l.add(career);
-        ca.setObjects(l);
+      super.onCreate(savedInstanceState);
 
-        //Setup the adapter views;
-        adapter = new CareersArrayListAdapter(this, R.layout.list_view_row);
-        adapter.setItems(ca.getCareers());
-        adapter.setContext(getBaseContext());
-        adapter.setParent(this);
-        lv = (ListView) findViewById(R.id.list);
-        lv.setAdapter(adapter);
-        
-        //Create an AsyncTask to update that data;
-        getDataTask = new GetCareersDataTask();
-        getDataTask.setContext(this);
-        getDataTask.setAdapter(adapter);
-        getDataTask.execute(careersURL, careersURL);
-        
-        final CareersListViewActivity context = CareersListViewActivity.this;
-        lv.setOnItemClickListener(new OnItemClickListener() {
+      requestWindowFeature(Window.FEATURE_NO_TITLE);
+      setContentView(R.layout.detailed_menu_list);    	
 
-            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+      TextView title = (TextView)findViewById(R.id.title_bar);
+      title.setText("Gun.io - Careers");
 
-                   final Intent i = new Intent(context, IndividualItemActivity.class);
-                   Career c = ca.getCareers().get(arg2);
-                   c = (Career) adapter.getCareer(arg2);
-                   
-                   if(c.getTitle().equals("Loading..")){
-                	   return;
-                   }
-                   
-                   i.putExtra("title", c.getTitle());
-                   i.putExtra("company_name", c.getCompany_name());
-                   i.putExtra("job_description", c.getJob_mkd());
-                   i.putExtra("about", c.getAbout_mkd());
-                   i.putExtra("skills", c.getSkills_mkd());
-                   i.putExtra("apply_url", c.getHomepage());
-                   i.putExtra("id", c.getId());
-                   i.putExtra("slug", c.getSlug());
-                   startActivity(i); 
-                
-            }});
-        
+      ArrayList<Career> l = new ArrayList<Career>();
+      final Careers ca = new Careers();
+      Career career = ca.new Career();
+      career.setTitle("Loading..");
+      City lo = new City();
+      lo.setName("Please wait..");
+      career.setCity(lo);
+      career.setCompany_name("");
+      l.add(career);
+      ca.setObjects(l);
+
+      //Setup the adapter views;
+      adapter = new CareersArrayListAdapter(this, R.layout.list_view_row);
+      adapter.setItems(ca.getCareers());
+      adapter.setContext(getBaseContext());
+      adapter.setParent(this);
+      lv = (ListView) findViewById(R.id.list);
+      lv.setAdapter(adapter);
+
+      //Create an AsyncTask to update that data;
+      getDataTask = new GetCareersDataTask();
+      getDataTask.setContext(this);
+      getDataTask.setAdapter(adapter);
+      getDataTask.execute(careersURL, careersURL);
+
+      final CareersListViewActivity context = CareersListViewActivity.this;
+      lv.setOnItemClickListener(new OnItemClickListener() {
+
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+          final Intent i = new Intent(context, IndividualItemActivity.class);
+          Career c = ca.getCareers().get(arg2);
+          c = (Career) adapter.getCareer(arg2);
+
+          if(c.getTitle().equals("Loading..")){
+            return;
+          }
+
+          String mkData = "###"+c.getTitle() + "\n";
+          if( c.getJob_mkd() != null )
+            mkData += c.getJob_mkd() + "\n";
+          if( c.getSkills_mkd() != null )
+            mkData += c.getSkills_mkd() + "\n";
+
+          i.putExtra("has_markup", true);
+          i.putExtra("mdv_data", mkData);
+          i.putExtra("post_type", IndividualItemActivity.POST_TYPE_CAREER);
+          i.putExtra("title", c.getTitle());
+          i.putExtra("company_name", c.getCompany_name());
+          i.putExtra("job_description", c.getJob_mkd());
+          i.putExtra("about", c.getAbout_mkd());
+          i.putExtra("skills", c.getSkills_mkd());
+          i.putExtra("apply_url", c.getHomepage());
+          i.putExtra("id", c.getId());
+          i.putExtra("slug", c.getSlug());
+
+          startActivity(i); 
+
+        }});
+
     }
-    
-    public void onResume() {
-        super.onResume();
-    }    
+
+  public void onResume() {
+    super.onResume();
+  }    
 
 }
--- src/io/gun/gunbot/IndividualItemActivity.java
+++ src/io/gun/gunbot/IndividualItemActivity.java
@@ -9,104 +9,127 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnClickListener;
 import android.view.View.OnFocusChangeListener;
 import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.util.Log;
+import us.feras.mdv.MarkdownView;
+import android.graphics.Color;
 
 public class IndividualItemActivity extends Activity {
-	
-	Context c;
-	
-	ImageButton careers;
-	ImageButton contracts;
-	ImageButton opensource;
 
-	private String apply_url;
+  final public static int POST_TYPE_CAREER = 0;
+  final public static int POST_TYPE_CONTRACT = 1;
+  final public static int POST_TYPE_OPEN = 2;
 
-	private Account[] accounts;
-	
-	
-    /** Called when the activity is first created. */
+  Context c;
+
+  ImageButton careers;
+  ImageButton contracts;
+  ImageButton opensource;
+
+  private String apply_url;
+
+  private Account[] accounts;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    requestWindowFeature(Window.FEATURE_NO_TITLE);
+    setContentView(R.layout.detailed_markup_item);
+    Intent i = this.getIntent(); 
+    
+    if ( i.getBooleanExtra("has_markup", false) ) {
+      LinearLayout llayout = (LinearLayout) findViewById(R.id.info_layout);
+      MarkdownView mdv = new MarkdownView(this);
+      mdv.setBackgroundColor(Color.TRANSPARENT);
+      if( llayout == null ) {
+        Log.d("gun.io","Couldn't fetch layout view");
+        finish();
+      }
+      llayout.addView(mdv, 0, new ViewGroup.LayoutParams(-1, -1));
+      String mdData = i.getStringExtra("mdv_data");
+      mdv.loadMarkDownData(mdData);
+      return;
+    }
+
+    setContentView(R.layout.detailed_item);
+
+
+    /** Assign layouts to views. */
+    TextView title = (TextView)findViewById(R.id.title);
+    TextView header = (TextView)findViewById(R.id.title_bar);
+
+    TextView title2 = (TextView)findViewById(R.id.title2);
+
+    TextView header1 = (TextView)findViewById(R.id.header1);
+    TextView text1 = (TextView)findViewById(R.id.text1);
+
+    TextView header2 = (TextView)findViewById(R.id.header2);
+    TextView text2 = (TextView)findViewById(R.id.text2);
+
+    TextView header3 = (TextView)findViewById(R.id.header3);
+    TextView text3 = (TextView)findViewById(R.id.text3);
+
+    TextView apply_button = (TextView)findViewById(R.id.apply_button);
+    apply_button.setOnClickListener(new ApplyButtonListener());
+
+    accounts = AccountManager.get(this).getAccounts();
+    TextView email_button = (TextView)findViewById(R.id.remind_button);
+    email_button.setOnClickListener(new EmailButtonListener());
+  }
+
+  private class EmailButtonListener implements OnClickListener {
+    /* Create the Intent */
+    final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.detailed_item);
-        
-        c = this;
+    public void onClick(View v) {
+      /* Fill intent with Data */
+      emailIntent.setType("plain/text");
 
-        TextView title = (TextView)findViewById(R.id.title);
-        TextView header = (TextView)findViewById(R.id.title_bar);
-        
-        TextView title2 = (TextView)findViewById(R.id.title2);
-        
-        TextView header1 = (TextView)findViewById(R.id.header1);
-        TextView text1 = (TextView)findViewById(R.id.text1);
-        
-        TextView header2 = (TextView)findViewById(R.id.header2);
-        TextView text2 = (TextView)findViewById(R.id.text2);
-        
-        TextView header3 = (TextView)findViewById(R.id.header3);
-        TextView text3 = (TextView)findViewById(R.id.text3);
-        
-        TextView apply_button = (TextView)findViewById(R.id.apply_button);
-        
-        apply_button.setOnClickListener(new ApplyButtonListener());
-        
-        accounts = AccountManager.get(this).getAccounts();
-        TextView email_button = (TextView)findViewById(R.id.remind_button);
-        email_button.setOnClickListener(new EmailButtonListener());
-        
+      String e = "";
+      for (Account account : accounts) {
+        String possibleEmail = account.name;
+        if(possibleEmail.contains("@")) {
+          e = possibleEmail;
+          break;
+        }
+      }
     }
 
     private void sendEmail() {
-      emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{e});
-      
+      emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{});
+
       emailIntent.putExtra(
           android.content.Intent.EXTRA_SUBJECT, 
-          "Remember to apply to " + title2_s + "!");
-      
+          "Remember to apply to " 
+         // + title2_s 
+          + "!");
+
       emailIntent.putExtra(
           android.content.Intent.EXTRA_TEXT,
-          "Hey!\n\nDon't forget to apply to " + title2_s 
-          + "!\n\nhttp://gun.io/careers/" + id + "/" + slug 
-          + "\n\nLove!,\nTeam Gun.io");
+      //    "Hey!\n\nDon't forget to apply to " + title2
+      //    + "!\n\nhttp://gun.io/careers/" + id + "/" + slug 
+           "\n\nLove!,\nTeam Gun.io");
 
       /* Send it off to the Activity-Chooser */
       startActivity(Intent.createChooser(emailIntent, "Send mail..."));
     }
+  }
 
-    private class EmailButtonListener extends OnClickListener {
-      @Override
-        public void onClick(View v) {
-          /* Create the Intent */
-          final Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
-
-          /* Fill it with Data */
-          emailIntent.setType("plain/text");
-
-          String e = "";
-          for (Account account : accounts) {
-            String possibleEmail = account.name;
-            if(possibleEmail.contains("@")) {
-              e = possibleEmail;
-              break;
-            }
-          }
-        }
+  private class ApplyButtonListener implements OnClickListener{
+    @Override
+    public void onClick(View v) {
+      Intent i = new Intent(Intent.ACTION_VIEW);
+      i.setData(Uri.parse(apply_url));
+      startActivity(i);
     }
-
-  private class ApplyButtonListener extends OnClickButtonListener{
-
-			@Override
-			public void onClick(View v) {
-				Intent i = new Intent(Intent.ACTION_VIEW);
-				i.setData(Uri.parse(apply_url));
-				startActivity(i);
-				
-			}
   }
 }
